a1bf8b23430e0557298a538e87ff13d07b241827
Justin Turner <jmturner638@gmail.com>
2021-07-31 16:35:30 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,25 @@
 def main():
     #write your code below this line
+    numbers = []
+
+    while True:
+        number = int(input())
+
+        if number == -1:
+            break
+
+        numbers.append(number)
+
+    average = sum(numbers) / len(numbers)
+    print("Average: " + str(average))
+
+        
+
+def sum(numbers):
+    result = 0
+    for number in numbers:
+        result += number
+    return result
 
 if __name__ == '__main__':
     main()
